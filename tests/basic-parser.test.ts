cbc486137fdaf6718e22082ed64d4d6fa6745bfc
Blane Henok <blanehenok@Tigists-MacBook-Air-3209.local>
2025-09-10 17:33:39 -0400

    Task A revised with test: wrong name but correct age, being corrected by removing toHaveLength line

--- tests/basic-parser.test.ts
+++ tests/basic-parser.test.ts
@@ -14,48 +14,45 @@ test("parseCSV yields arrays", async () => {
   expect(results[4]).toEqual(["Nim", "22"]);
 });
 
-test("parseCSV yields arrays", async () => {
+test(" wrong name but correct age", async () => {
   const results = await parseCSV(PEOPLE_CSV_PATH)
-  expect(results).toHaveLength(5);
   expect(results[1]).not.toEqual(["Ashley", "23"]); //testing wrong name but correct age should not be equal
 });
 
-test("parseCSV yields arrays", async () => {
+test("wrong age but correct name", async () => {
   const results = await parseCSV(PEOPLE_CSV_PATH)
   expect(results[3]).not.toEqual(["Charlie", "27"]);  //testing wrong age but correct name should not be equal
 });
 
-test("parseCSV yields arrays", async () => {
+test("wrong age and wrong name", async () => {
   const results = await parseCSV(PEOPLE_CSV_PATH)
   expect(results[4]).not.toEqual(["Nim", "22"]);  //testing wrong age and name should not be equal
 });
 
-test("parseCSV yields arrays", async () => {
+test("wrong row number", async () => {
   const results = await parseCSV(PEOPLE_CSV_PATH)
   expect(results[2]).not.toEqual(["Nim", "22"]); //testing wrong row number should not be equal
 });
 
-test("parseCSV yields arrays", async () => {
+test("wrong row number that does not exist", async () => {
   const results = await parseCSV(PEOPLE_CSV_PATH)
   expect(results[7]).not.toEqual(["Nim", "22"]); //testing wrong row number that does not exist should not be equal
 });
 
-test("parseCSV yields arrays", async () => {
+test("double quotation", async () => {
   const results = await parseCSV(PEOPLE_CSV_PATH)
   expect(results[3]).not.toEqual(["Liya Johnson", "24"]);  //testing case with spacing grouped by quotation marks should be equal
 });
 
-test("parseCSV yields arrays", async () => {
+test("no age", async () => {
   const results = await parseCSV(PEOPLE_CSV_PATH)
   expect(results[3]).not.toEqual(["Samara", null]);  //testing case with no age being defaulted to null should be equal
 });
 
-test("parseCSV yields arrays", async () => {
+test("age written as decimal", async () => {
   const results = await parseCSV(PEOPLE_CSV_PATH)
   expect(results[3]).not.toEqual(["Charlie", "27.0"]);  //testing age written as decimal should be equal
 });
-
-
 
 test("parseCSV yields only arrays", async () => {
   const results = await parseCSV(PEOPLE_CSV_PATH)
